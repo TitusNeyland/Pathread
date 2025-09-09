5575269ec10ef297086a0cd1bdba49ef24285434
TitusNeyland <neylandtitus@yahoo.com>
2025-09-09 11:32:42 -0500

    Initial commit

---
+++ Pathread/PathreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathreadApp.swift
+//  Pathread
+//
+//  Created by Titus Neyland on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PathreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PathreadTests/PathreadTests.swift
@@ -0,0 +1,17 @@
+//
+//  PathreadTests.swift
+//  PathreadTests
+//
+//  Created by Titus Neyland on 9/9/25.
+//
+
+import Testing
+@testable import Pathread
+
+struct PathreadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
