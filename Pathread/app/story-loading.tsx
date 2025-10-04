467f8d3607dcffed605d2f4aec4bec6981f22d10
Titus Neyland <neylandtitus@yahoo.com>
2025-10-04 13:36:25 -0500

    [titusan] routing

---
+++ Pathread/app/story-loading.tsx
@@ -0,0 +1,2 @@
+// Re-export the StoryLoading screen for expo-router
+export { default } from './screens/StoryLoading/StoryLoadingScreen';
---
+++ Pathread/app/story-reader.tsx
@@ -0,0 +1,2 @@
+// Re-export the StoryReader screen for expo-router
+export { default } from './screens/StoryReader/StoryReaderScreen';
